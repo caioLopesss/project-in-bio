d952fba5eab2c1d1b6e18f33a4af0a3afd2d9617
Caio Lopes <caiolopeis95@gmail.com>
2025-04-02 11:28:45 -0300

    refactor: configuraçao auth google

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,13 +1,19 @@
 import NextAuth from "next-auth";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 import { firebaseCert } from "./firebase";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: FirestoreAdapter({
     credential: firebaseCert,
   }),
-  providers: [Google],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
   events: {},
   callbacks: {},
 });
