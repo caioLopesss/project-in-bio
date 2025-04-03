f7ed672b38dbda576d42f0ec2d2162a3a593a646
Caio Lopes <caiolopeis95@gmail.com>
2025-04-03 09:57:31 -0300

    refactor:configurando auth

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,6 +1,10 @@
+import { auth } from "@/app/lib/auth";
 import Button from "../ui/button";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth;
+
+  console.log(session);
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
       <h3 className="Text-white text-2xl font-bold ">ProjectInBio</h3>
--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -2,6 +2,24 @@ import "server-only";
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
+
+console.log(
+  "FIREBASE_PROJECT_ID:",
+  process.env.FIREBASE_PROJECT_ID ?? "❌ NÃO DEFINIDO"
+);
+console.log(
+  "FIREBASE_CLIENT_EMAIL:",
+  process.env.FIREBASE_CLIENT_EMAIL ?? "❌ NÃO DEFINIDO"
+);
+console.log(
+  "FIREBASE_PRIVATE_KEY:",
+  process.env.FIREBASE_PRIVATE_KEY ? "✅ LOADED" : "❌ NÃO DEFINIDO"
+);
+console.log(
+  "FIREBASE_STORAGE_BUCKET:",
+  process.env.FIREBASE_STORAGE_BUCKET ?? "❌ NÃO DEFINIDO"
+);
+
 //certificado
 const decodedKey = Buffer.from(
   process.env.FIREBASE_PRIVATE_KEY!,
